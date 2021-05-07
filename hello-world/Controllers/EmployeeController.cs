da0395f97d99821939b95b4e58efedc1fa7445ff
mikevu23 <43010987+mikevu23@users.noreply.github.com>
2021-05-07 12:26:30 -0700

    Added employee controller, cors, employee class

---
+++ hello-world/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using hello_world.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hello_world.Controllers
+{
+
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet("api/employees")]
+        public ActionResult GetEmployees() {
+            List<Employee> employees = new List<Employee>();
+            List<string> names = new List<string>{ "Michael", "Vince", "Lizet", "Ivette", "Jimy", "Bob", "Tom"};
+            for (int i = 0; i < names.Count; i++) {
+                Employee employee = new Employee {
+                    Name = names[i],
+                    Number = i
+                };
+
+                employees.Add(employee);
+            }
+
+            return Ok(employees);
+        }
+    }
+}
